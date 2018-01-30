c443872794529a8c9e9be362ef1b119667337a79
Sergey <sergeyganous@gmail.com>
2018-01-30 13:04:40 -0800

    Changed Post Login Route Issure #16
    * Designated the job_posting index page to act as the main dashboard.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
-    redirect_to user_path(@user)
+    redirect_to job_postings_path
   end
 
   def destroy
