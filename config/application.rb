2e0509f59997e407304eda93da1aae42133a6571
“s1v4” <hdao61@gmail.com>
2018-02-22 14:48:42 -0800

    Set uuid as default for models and future migrations.

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,10 @@ module Limelight
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # Use uuid by default. 
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
