b50c0af712fa987fc0e1d307f9a0efe002ea4a80
“s1v4” <hdao61@gmail.com>
2018-03-12 22:20:35 -0700

    fix static pages indentation and permissions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,21 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :ensure_signed_in, only: [:landing, :authentication]
+  skip_before_action :ensure_signed_in
+
   def landing
   end
+
   def authentication
   end
+
   def pricing
   end
+
   def contact_us
   end
+
   def about_us
   end
+
   def services
   end
 end
