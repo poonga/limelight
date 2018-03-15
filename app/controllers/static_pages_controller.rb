85796759f86eb2eb5698c3ea3ad5830e89794eb3
Sergey <sergeyganous@gmail.com>
2018-03-15 11:27:15 -0700

    Added Global Navbar to Application [Issue #38]
    * Created a user nav bar and a gues navbar that is rendered onn the
    application level.
    * Added functionality where if user is signed in, they are redirected to
    their dashboard when visiting the landing page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   skip_before_action :ensure_signed_in, only: [:landing, :authentication]
   def landing
+    if user_signed_in?
+      redirect_to company_job_postings_path(current_user[:company_id])
+    end
   end
   def authentication
   end
