8962c651eb68fdc0e9c5c9ec2a5569a145506ef9
Minh Huynh <mnthuynh@gmail.com>
2018-04-10 12:46:01 -0700

    Issue #49 Modify if statement

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,15 +32,9 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      if (user.first_name == nil)
-        user.first_name = auth.info.first_name
-      end
-      if (user.last_name == nil)
-        user.last_name = auth.info.last_name
-      end
-      if (user.email == nil)
-        user.email = auth.info.email
-      end
+      user.first_name = auth.info.first_name if user.first_name.nil?
+      user.last_name = auth.info.last_name if user.last_name.nil?
+      user.email = auth.info.email if user.email.nil?
       user.icon = auth.info.image
       company_name = auth.extra.raw_info.hd ? auth.extra.raw_info.hd.split('.')[0] : 'Independent Agency'
       user.company = Company.find_or_create_by(name: company_name)
