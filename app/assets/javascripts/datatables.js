8a60cbf246bc20fd8abf74bca5d7444d27d5032a
“s1v4” <hdao61@gmail.com>
2018-04-15 00:04:14 -0700

    change table options and display

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -1,4 +1,20 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#job-postings-table').dataTable({
+    pageLength: 10,
+    lengthMenu: [
+      [ 10, 25, 50, -1 ],
+      [ '10 listings', '25 listings', '50 listings', 'Show all' ]
+    ],
+    ordering: false
+  });
+
+  $('#applicants-table').dataTable({
+    pageLength: 25,
+    lengthMenu: [
+      [ 25, 50, 100, -1 ],
+      [ '25 applicants', '50 applicants', '100 applicants', 'Show all' ]
+    ],
+    ordering: false
+  });
 });
